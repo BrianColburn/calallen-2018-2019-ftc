75d4a0b864fdcda54bea5862156ad5eef63a1a74
CalallenHP <a@b.c>
2019-03-18 15:20:47 -0500

    Tidy up
    -- Brian

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.kt
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode
 
 import android.hardware.SensorManager
@@ -40,20 +11,8 @@ import com.qualcomm.robotcore.util.ElapsedTime
 
 import java.util.ArrayList
 
-import java8.util.Optional
-
 /**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Bare minimum manual control opmode.
  */
 
 @TeleOp(name = "Manual", group = "Iterative Opmode")
@@ -100,32 +59,14 @@ class Manual : OpMode() {
             }
         }
 
-        if (c is BoysController && ser[0] is Servo) {
-            //ser[0].scaleRange(0,.75);
-        }
-
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         mot[0]?.direction = DcMotorSimple.Direction.REVERSE
         mot[3]?.direction = DcMotorSimple.Direction.REVERSE
 
-        /*foo = new Foo(telemetry);
-        sensorManager = (SensorManager)new Activity().getSystemService(Context.SENSOR_SERVICE);
-        Sensor gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        Sensor asensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Sensor msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        sensorManager.registerListener(foo, gsensor, SensorManager.SENSOR_DELAY_GAME);
-        sensorManager.registerListener(foo, asensor, SensorManager.SENSOR_DELAY_GAME);
-        sensorManager.registerListener(foo, msensor, SensorManager.SENSOR_DELAY_GAME);*/
-
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized")
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    override fun init_loop() {}
 
     /*
      * Code to run ONCE when the driver hits PLAY
@@ -180,10 +121,5 @@ class Manual : OpMode() {
         telemetry.addData("Motors", "left (%.2f), right (%.2f), hook (%.2f)", leftPower, rightPower, hookPow)
         telemetry.addData("Servo", "(%.0f) Degrees, actually (%.2f)", servoPos * 180, servoPos)
     }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    override fun stop() {}
 
 }
